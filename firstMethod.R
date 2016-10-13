e5f362baf8003b9dc7e81850b81571ea665e5cb7
Andrey Litvinov <litvinovandrey@list.ru>
2016-10-13 20:48:13 +0500

    complite first method without testing

--- firstMethod.R
+++ firstMethod.R
@@ -3,11 +3,12 @@ library("ggplot2")
 library("GGally")
 library("psych")
 library(gdata)
+library(Metrics)
 
 
 step = 10
 M = 30
-k = 0
+P = 10
 
 data = read.csv("C:/my/econometrics/data.csv")
 
@@ -61,7 +62,68 @@ GetIndexStartMostSimilarSample <- function(trainData, approximationSample, step)
   return(indexMaxCorrelation)
 }
 
+GetCountPositive <- function(data)
+{
+  return(sum(data >= 0))
+}
+
+
+GetNewHistoryData <- function(trainData, index, countPositive, countNegative)
+{
+  indexPos <- 0
+  indexNeg <-  0
+  newData <-  c()
+  i <-  1
+  j <- 0
+  while((indexPos < countPositive) | (indexNeg < countNegative))
+  {
+    if((trainData[index] >= 0) & (indexPos < countPositive)) 
+    {
+      newData[i] <-  trainData[index]
+      indexPos <- indexPos + 1
+      i <-  i + 1
+    }
+    if((trainData[index] < 0) & (indexNeg < countNegative)) 
+    {
+      newData[i] <-  trainData[index]
+      indexNeg <- indexNeg + 1
+      i <-  i + 1
+    }
+    index <- index + 1
+  }
+  return(newData)
+}
+
+GetStartPredictPoint <- function(trainData, index, countPositive, countNegative)
+{
+  indexPos <- 0
+  indexNeg <-  0
+  newData <-  c()
+  i <-  1
+  j <- 0
+  while((indexPos < countPositive) | (indexNeg < countNegative))
+  {
+    if((trainData[index] >= 0) & (indexPos < countPositive)) 
+    {
+      newData[i] <-  trainData[index]
+      indexPos <- indexPos + 1
+      i <-  i + 1
+    }
+    if((trainData[index] < 0) & (indexNeg < countNegative)) 
+    {
+      newData[i] <-  trainData[index]
+      indexNeg <- indexNeg + 1
+      i <-  i + 1
+    }
+    index <- index + 1
+  }
+  return(index + 1)
+}
+
 dataDelta <- GetDelta(data$value)
+
+train <- data$value[1:round(length(data$value)*0.4)]
+test <- data$value[(round(length(data$value)*0.4) + 1): round(length(data$value)*0.8)]
 
 trainDelta <- dataDelta[1:round(length(data$value)*0.4)] #0:40%
 testDelta <- dataDelta[(round(length(data$value)*0.4) + 1): round(length(data$value)*0.8)] #40:80%
@@ -72,12 +134,64 @@ validationDelta <- dataDelta[round(length(data$value)*0.8): length(data$value)] 
 plot(trainDelta[1:100], type = "l")
 
 
-#1
+
 trainData <- GetTrainSample(trainDelta, M)
 approxSample <- GetAproxSample(trainDelta, M)
 length(trainData)
 
-#3
+
 similarIndex <- GetIndexStartMostSimilarSample(trainData, approxSample, 5)
 
+plot(approxSample, type = 'l')
+plot(trainData[similarIndex: (similarIndex + M)], type = 'l')
+
+countPositiveM <- GetCountPositive(approxSample)
+countNegativeM <- M - countPositiveM
+
+countPositiveOffsetSample <- GetCountPositive(trainData[similarIndex: (similarIndex + M - 1)])
+countNegativeOffsetSample <- M - countPositiveOffsetSample
+
+if(countPositiveM != countPositiveOffsetSample)
+{
+  print("Warning : Incorrect size approximation and history data")
+  print(paste("approximation positive : ", toString (countPositiveM), sep = ""))
+  print(paste("history positive : ", toString (countPositiveOffsetSample), sep = ""))
+}
+
+newData <- GetNewHistoryData(trainDelta, similarIndex, countPositiveM, countNegativeM)
+startPredictPoint <- GetStartPredictPoint(trainDelta, similarIndex, countPositiveM, countNegativeM)
+
+dataForLmModel <- data.frame(M = approxSample, history = newData)
+
+modelPos <- lm(M ~ history, data=subset(dataForLmModel, M >= 0))
+modelPos$coefficients[1] #offset
+modelPos$coefficients[2] #k
+
+modelNeg <- lm(M ~ history, data=subset(dataForLmModel, M < 0))
+modelNeg$coefficients[1] #offset
+modelNeg$coefficients[2] #k
+
+predictedValue = c(last(train))
+
+j <- 2
+for( i in startPredictPoint:(startPredictPoint + P - 1))
+{
+  if(trainData[i] >= 0)
+  {
+    predictedValue[j] <- predictedValue[j - 1] +  trainData[i] * modelPos$coefficients[2] + modelPos$coefficients[1]
+  }
+  else
+  {
+    predictedValue[j] <- predictedValue[j - 1] +  trainData[i] * modelNeg$coefficients[2] + modelNeg$coefficients[1]
+  }
+  j <- j + 1
+}
+
+predictedValue <- predictedValue[c(2:11)]
+
+
+plot(predictedValue, type = 'l')
+plot(test[1:10], type = 'l')
+
+mae(predictedValue, test[1:10])
 
